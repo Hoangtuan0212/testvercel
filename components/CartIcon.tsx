7cbf7670fb92704a3df6f59259ff7c3cb35039de
Hoangtuan0212 <nguyenhoangtuan021203@gmail.com>
2025-09-27 20:24:38 +0700

    Update CartIcon.tsx

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -1,16 +1,24 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import CartPopup from "./CartPopup";
 import { useCart } from "../context/CartContext";
 
-const CartIcon: React.FC = () => {
+interface CartIconProps {
+  onClick?: () => void;
+}
+
+const CartIcon: React.FC<CartIconProps> = ({ onClick }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const cartPopupRef = useRef<HTMLDivElement>(null);
 
-  const { totalQuantity } = useCart();
+  const { cart } = useCart();
+  const totalQuantity = cart?.totalQuantity ?? 0;
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (cartPopupRef.current && !cartPopupRef.current.contains(event.target as Node)) {
+      if (
+        cartPopupRef.current &&
+        !cartPopupRef.current.contains(event.target as Node)
+      ) {
         setIsCartOpen(false);
       }
     };
@@ -18,19 +26,30 @@ const CartIcon: React.FC = () => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
+  const handleClick = () => {
+    setIsCartOpen((prev) => !prev);
+    onClick?.();
+  };
+
   return (
     <div className="relative">
-      <button onClick={() => setIsCartOpen(prev => !prev)} className="relative focus:outline-none">
-        <img src="/images/icon/carticon.png" alt="Giỏ hàng" className="w-8 h-8" />
+      <button onClick={handleClick} className="relative focus:outline-none">
+        <img
+          src="/images/icon/carticon.png"
+          alt="Giỏ hàng"
+          width={25}
+          height={25}
+          className="w-8 h-8 object-contain bg-transparent border-none"
+        />
         {totalQuantity > 0 && (
-          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full px-1 min-w-[18px] text-center">
+          <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full px-1">
             {totalQuantity}
           </span>
         )}
       </button>
 
       {isCartOpen && (
-        <div ref={cartPopupRef} className="absolute right-0 mt-2 z-50 shadow-lg bg-white rounded-lg">
+        <div ref={cartPopupRef} className="absolute z-50">
           <CartPopup onClose={() => setIsCartOpen(false)} />
         </div>
       )}
